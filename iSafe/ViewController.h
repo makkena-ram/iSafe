836a74ee174f4157d5dac0d7694ee9742e6badc2
Ramakrishna Makkena <ramakrishnamakkena@Ramakrishnas-MacBook-Pro.local>
2014-11-08 11:22:23 -0600

    Initial Commit

---
+++ iSafe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iSafe
+//
+//  Created by Ramakrishna Makkena on 11/8/14.
+//  Copyright (c) 2014 nwmissouri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iSafe/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iSafe
+//
+//  Created by Ramakrishna Makkena on 11/8/14.
+//  Copyright (c) 2014 nwmissouri. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
